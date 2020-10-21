39213e4a8e9bc671062439c6c42030c5b1a30094
hooty868 <hooty868@gmail.com>
2020-10-21 16:29:01 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const app = express()
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
+const exphbs = require('express-handlebars')
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+
 const port = 3000
 // ----------------set environment-------------------- //
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 app.listen(port, () => {
